92e13acbe6bf550fae2222911adcf82150cf2ca1
Hussiendev <hussienzoughaib@gamil.com>
2025-05-30 21:49:25 +0300

    git commit 'a';

---
+++ controller/authController.js
@@ -0,0 +1,12 @@
+import { User } from "../modules/User";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { sendWelcomeEmail } from "../config/Nodmailer.js";
+export const register=async(req,res)=>{
+    try{
+
+    }
+    catch(){
+        
+    }
+}
