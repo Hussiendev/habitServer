bceecba1be3e86c6cd8a61ae767588afa0459aec
Hussiendev <hussienzoughaib@gamil.com>
2025-05-31 22:34:04 +0300

    git commit 'a';

--- controller/authController.js
+++ controller/authController.js
@@ -1,12 +1,30 @@
-import { User } from "../modules/User";
+import { User } from "../modules/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { sendWelcomeEmail } from "../config/Nodmailer.js";
 export const register=async(req,res)=>{
     try{
-
-    }
-    catch(){
+        const {name,email,password}=req.body;
+        if(!name | !email | !password){
+            return res.json({message:'all feilds are required'});
         
+        }
+        const targetuser=await User.findOne({email});
+        if(targetuser){
+            return res.json({message:'user arleady exist'});
+
+        }
+        const hashedpass=await bcrypt.hash(password,10);
+        const user=await User.create({name,email,password:hashedpass});
+await user.save();
+
+        return res.status(200).json({user});
+
+    
+        
+    }
+    catch(eror){
+        console.log(error);
+        return res.status(404).json({message:'error server '});
     }
 }
--- server.js
+++ server.js
@@ -3,18 +3,16 @@ import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { DbConnect } from './db/db.config.js';
+import authRouts from './routes/authRouts.js';
 
 dotenv.config();
 const app = express();
 app.use(express.json());//for json data
 app.use(cookieParser());//for cookies
 const Port=process.env.PORT || 4000;
+app.use('/api/auth/',authRouts);
 
 app.listen(Port,()=>{
     DbConnect();
     console.log(`app is runed at port ${Port}`);
 });
-app.get("/",(req,res)=>{
-    return res.json({message:'hi'});
-    
-})
